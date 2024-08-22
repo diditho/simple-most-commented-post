09f08e2cc14d17e3e6cd622b6948146614a010e2
didit s.banuardi.n <diditho@users.noreply.github.com>
2024-08-22 22:44:37 +0700

    remove admin section

--- includes/class-simple-most-commented-post.php
+++ includes/class-simple-most-commented-post.php
@@ -11,24 +11,15 @@ class Simple_Most_Commented_Post {
         $this->version = SIMPLE_MOST_COMMENTED_POST_VERSION;
 
         $this->load_dependencies();
-        $this->define_admin_hooks();
         $this->define_public_hooks();
     }
 
     private function load_dependencies() {
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-simple-most-commented-post-loader.php';
-        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-simple-most-commented-post-admin.php';
         require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-simple-most-commented-post-public.php';
         require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-simple-most-commented-post-widget.php';
 
         $this->loader = new Simple_Most_Commented_Post_Loader();
-    }
-
-    private function define_admin_hooks() {
-        $plugin_admin = new Simple_Most_Commented_Post_Admin($this->get_plugin_name(), $this->get_version());
-        $this->loader->add_action('widgets_init', $plugin_admin, 'register_widget');
-        $this->loader->add_action('admin_menu', $plugin_admin, 'add_plugin_admin_menu');
-        $this->loader->add_action('admin_init', $plugin_admin, 'register_settings');
     }
 
     private function define_public_hooks() {
