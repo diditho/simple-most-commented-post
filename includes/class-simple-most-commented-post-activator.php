d089c3e13802423e58b321143ff672626b953683
didit s.banuardi.n <diditho@users.noreply.github.com>
2024-08-22 15:17:07 +0700

    Update class-simple-most-commented-post-activator.php

--- includes/class-simple-most-commented-post-activator.php
+++ includes/class-simple-most-commented-post-activator.php
@@ -1,1 +1,11 @@
+<?php
+
+class Simple_Most_Commented_Post_Activator {
+
+    public static function activate() {
+        if (get_option('simple_most_commented_post_count') === false) {
+            update_option('simple_most_commented_post_count', 5);
+        }
+    }
+}
 
