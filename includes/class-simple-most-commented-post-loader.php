eeeec3ab49629add1cefccc200c0a1bbfa963474
didit s.banuardi.n <diditho@users.noreply.github.com>
2024-08-22 15:03:59 +0700

    Create class-simple-most-commented-post-loader.php

---
+++ includes/class-simple-most-commented-post-loader.php
@@ -0,0 +1,24 @@
+<?php
+
+class Simple_Most_Commented_Post_Loader {
+
+    protected $actions;
+
+    public function __construct() {
+        $this->actions = array();
+    }
+
+    public function add_action($hook, $component, $callback) {
+        $this->actions[] = array(
+            'hook' => $hook,
+            'component' => $component,
+            'callback' => $callback
+        );
+    }
+
+    public function run() {
+        foreach ($this->actions as $hook) {
+            add_action($hook['hook'], array($hook['component'], $hook['callback']));
+        }
+    }
+}
